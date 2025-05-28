2e96c2eb61853398571ff2e88d323e0c13d5ce21
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-05-28 18:36:06 -0300

    Update Atividade_14.js

--- Atividade_14.js
+++ Atividade_14.js
@@ -1,8 +1,12 @@
-function buscarElemento(){
-    const elemento = document.getElementById("localdoElemento").value;
-    for(let item of minhaFila){
-        if(elemento === item.elemento){
-            alert("O elemento está na fila circular");
-        }
-    }
+ buscarElemento(dado){
+    pos = this.inicio;
+     for(let i = 0;i < this.qtd;i++){
+         if(this.elementos[pos] === dado)
+             return pos;
+    if(pos < elementos[i] - 1)
+        pos++;
+    else
+        pos = 0;
+     }
+     return -1;
 }
