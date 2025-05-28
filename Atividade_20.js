da52fd0bd179124148fcd1cb6d59e37784aabb5b
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-05-28 18:53:42 -0300

    Create Atividade_20.js

---
+++ Atividade_20.js
@@ -0,0 +1,10 @@
+function mostraaTopoPilhaEncadeada(){
+  return this.#peek();
+}
+// código do peek
+/*public T peek(){
+        if(!isEmpty()){
+            return this.topo.getDado();
+        }
+        return null;
+    }*/
