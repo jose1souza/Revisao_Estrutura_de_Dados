8cb72fa8070441e7dadef0de242a92a7695d9cef
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-05-27 21:42:51 -0300

    Update Atividade_15.js
    Mudança de function para metod

--- Atividade_15.js
+++ Atividade_15.js
@@ -1,14 +1,16 @@
-function mostraPosicaoValor(){
-  const valor = document.getElementById("txtnovoValor").value;
+mostraPosicaoValor(valor){
   let encontrou = false;
   let posicao = 0;
-  for(const item of minhaFila){
+  let aux = this.#inicio;
+
+  while(aux != null){
     posicao++;
-    if(item.valor === valor){
+    if(aux.dado === valor){
       encontrou = true;
       alert("Valor está na Fila! Posição: "+ posicao);
       break;
     }
+    aux = aux.proximo;
   }
   if(encontrou === false)
       alert("Valor não encontrado na fila!");
