c0b16fe81fbe7629cecc2269a7310929fb73bf38
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-05-29 07:21:01 -0300

    Create Atividade_18.js

---
+++ Atividade_18.js
@@ -0,0 +1,10 @@
+converteDecimal(numeroUm,base){
+  let pilha = [];
+  while(numeroUm > 0){
+    pilha.push(numeroUm % base);
+    numeroUm = Math.floor(numeroUm / base);
+}
+  while(pilha.length > 0){
+    console.log(pilha.pop());
+}
+  }
